fd58e78c4ea73391244dda641ad49b4599bf9310
BananaKun <yanxun951224@gmail.com>
2021-08-27 11:00:02 +0100

    header & footer done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-
 import './App.css';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <>
       <Navbar />
-      {/*<Home />
-      <Footer /> */}
+      {/* <Home /> */}
+      <Footer />
 
     </>
   );
