5f2755487bf8e3405b885055b123e119c35750b0
BananaKun <yanxun951224@gmail.com>
2021-08-27 11:47:18 +0100

    create new components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Body from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar />
       {/* <Home /> */}
+      <Body />
       <Footer />
 
     </>
